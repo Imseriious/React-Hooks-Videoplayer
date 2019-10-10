6e666c659061996cc11b0042e1a8adb855a08995
Sammy.p <samueledi80@hotmail.com>
2019-10-10 13:48:18 +0200

    Containers prepared

---
+++ src/components/containers/SamPlayer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Video from '../Video';
+import Playlist from './Playlist';
+
+const SamPlayer = props => {
+    return (
+        <>
+            <Video />
+            <Playlist />
+        </>
+    );
+}
+
+export default SamPlayer;
