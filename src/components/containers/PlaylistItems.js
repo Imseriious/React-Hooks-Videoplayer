5c0b89d0b08b24a900c30229ee5aab5b8669474c
Sammy.p <samueledi80@hotmail.com>
2019-10-11 14:58:09 +0200

    HOC WithLink

--- src/components/containers/PlaylistItems.js
+++ src/components/containers/PlaylistItems.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PlaylistItem from '../PlaylistItem';
 import StyledPlaylistitems from '../styles/StyledPlaylistitems';
+import withLink from '../hoc/withLink';
+
+const PlaylistItemWithLink = withLink(PlaylistItem);
 
 const PlaylistItems = ({videos, active}) => (
     <StyledPlaylistitems>
         {videos.map(video => (
-            <PlaylistItem
-            key={videos.id}
+            <PlaylistItemWithLink
+            key={video.id}
             video={video}
             active={video.id === active.id ? true : false}
             played={video.played}
